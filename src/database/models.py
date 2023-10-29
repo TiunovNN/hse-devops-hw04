36d63b20ec2b961b591bf9fe678ed64c5198a285
TiunovNN <Tiunov.li.174@gmail.com>
2023-10-29 18:22:09 +0300

    Updating init sql (#9)
    * Update init.sql.

--- src/database/models.py
+++ src/database/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Enum, Integer, String
+from sqlalchemy import Column, Enum, Identity, Integer, String
 
 from schemas import DogType
 from .base_class import Base
@@ -7,13 +7,23 @@ from .base_class import Base
 class DogModel(Base):
     __tablename__ = 'dogs'
 
-    pk = Column(Integer, primary_key=True, index=True)
+    pk = Column(
+        Integer,
+        Identity(start=0, minvalue=0, cycle=True),
+        primary_key=True,
+        index=True,
+    )
     name = Column(String(100))
-    kind = Column(Enum(DogType))
+    kind = Column(Enum(DogType), index=True)
 
 
 class TimestampModel(Base):
     __tablename__ = 'timestamps'
 
-    id = Column(Integer, primary_key=True, index=True)
+    id = Column(
+        Integer,
+        Identity(start=0, minvalue=0, cycle=True),
+        primary_key=True,
+        index=True,
+    )
     timestamp = Column(Integer)
